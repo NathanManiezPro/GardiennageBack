330e1b36c46ce824e8196d518a5c56aee653d1a9
NathanManiezPro <Nathanmaniez@gmail.com>
2025-06-30 23:18:07 +0200

    Ajout de la fonctionnalité réservation : Prisma, contrôleurs, routes

--- src/routes/reservations.js
+++ src/routes/reservations.js
@@ -1,11 +1,12 @@
+// src/routes/reservations.js
 const express = require('express');
 const router = express.Router();
-const ctrl = require('../controllers/reservationsController');
+const reservationsController = require('../controllers/reservationsController');
 
-router.get('/', ctrl.getAll);
-router.get('/:id', ctrl.getById);
-router.post('/', ctrl.create);
-router.put('/:id', ctrl.update);
-router.delete('/:id', ctrl.delete);
+router.get('/', reservationsController.getAll);
+router.get('/:id', reservationsController.getById);
+router.post('/', reservationsController.create);
+router.put('/:id', reservationsController.update);
+router.delete('/:id', reservationsController.delete);
 
 module.exports = router;
